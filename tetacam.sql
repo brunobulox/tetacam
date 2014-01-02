9c14d9ee7f6047d99fdc358c69de2428d6238687
Mark H <brunobulox@yahoo.com>
2014-01-02 09:53:40 -0500

    added database sql

---
+++ tetacam.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.7.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 02, 2014 at 09:47 AM
+-- Server version: 5.5.33
+-- PHP Version: 5.4.4-14+deb7u7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tetacam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `name` varchar(35) NOT NULL,
+  `id` varchar(35) NOT NULL,
+  `pw` varchar(20) NOT NULL,
+  `comment` varchar(60) NOT NULL,
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `phone` text NOT NULL,
+  `phoneactive` tinyint(1) NOT NULL,
+  `text` varchar(40) DEFAULT NULL,
+  `textactive` tinyint(1) NOT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `emailactive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`userid`),
+  UNIQUE KEY `text` (`text`,`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
