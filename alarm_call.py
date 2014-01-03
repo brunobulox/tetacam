7b0723770f13bd62d8730398ef9e26be5687ee69
Mark H <brunobulox@yahoo.com>
2014-01-02 22:38:12 -0500

    rework toward adding email, upload and text

--- alarm_call.py
+++ alarm_call.py
@@ -7,19 +7,40 @@ import MySQLdb
 class AlarmCall:
 	def __init__(self):
 	    db = MySQLdb.connect("localhost","teta","tetacam","tetacam")
-	    sql = "select phone,userid from contacts where phoneactive=true"
+	    sql_phone = "select phone,userid from contacts where phoneactive=true"
+            sql_acct = "select id,pw from accounts where active=true"
+	    sql_email = "select email from contacts where emailactive=true"
 	    cursor = db.cursor()
-	    cursor.execute(sql)
-	    data = cursor.fetchall()
-	    db.close()
+	    cursor.execute(sql_phone)
+	    phone_data = cursor.fetchall()
+	    cursor.execute(sql_acct)
+            acct_data = cursor.fetchall()
+            cursor.execute(sql_email)
+            email_data = cursor.fetchall()
+            db.close()
 
-	    for row in data:
+	    for row in phone_data:
 #		print("%s" % row[0])
 		phonenumber = "%s" % row[0]
 		id = "%d" % row[1]
 		t=str(time.time())
 		self._write_call_file(phonenumber,id,t)
+	   
+	    for row in acct_data:
+	        print("%s" % row[0])
+		use_id = "%s" % row[0]
+		passwd = "%s" % row[1]
+		t=str(time.time())
+		#self._write_call_file(phonenumber,id,t)
 
+            for row in email_data:
+	        print("%s" % row[0])
+		use_id = "%s" % row[0]
+		t=str(time.time())
+		#self._write_call_file(phonenumber,id,t)
+
+
+	
 	def _write_call_file(self,phonenumber,id,t):
 	#write and move a file for asterisk to pick up to call out with
 		f=open("/tmp/alarm-" + t + id, "w")
@@ -34,6 +55,26 @@ class AlarmCall:
 		f.close()
 		shutil.move(f.name, '/var/spool/asterisk/outgoing/')
 #		print("moving %s" % phonenumber)
+
+	def _send_email(self,msg,imgpath):
+            senddate=datetime.strftime(datetime.now(), '%Y-%m-%d')
+            # Read a file and encode it into base64 format
+            fo = open(imgpath, "rb")
+            filecontent = fo.read()
+            encodedcontent = base64.b64encode(filecontent)  # base64
+            imgfile=os.path.basename(imgpath)
+            marker = "AUNIQUEMARKER"
+            p1="Date: %s\r\nFrom: %s <%s>\r\nTo: %s\r\nSubject: %s\r\nContent-Type: multipart/mixed; boundary=%s\r\n--%s\r\n" % (senddate, self.from_name, self.sender, self.recipient, self.subject, marker, marker)
+            p2="Content-Type: text/plain\r\nContent-Transfer-Encoding:8bit\r\n\r\n%s\r\n--%s\r\n" % (msg, marker)
+            p3="Content-Type: multipart/mixed; name=""%s""\r\nContent-Transfer-Encoding:base64\r\nContent-Disposition: attachment; filename=%s\r\n\r\n%s\r\n--%s--\r\n" % (imgfile, imgfile, encodedcontent, marker)
+
+            server = smtplib.SMTP('smtp.gmail.com:587')
+            server.starttls()
+            server.login(self.username, self.password)
+            # server.sendmail(self.sender, self.recipient, p1+p2+p3)
+	    # change made here to not attach file so this can be sent to sms
+	    server.sendmail(self.sender, self.recipient, p1+p2)
+            server.quit()
 
 
 try:   
