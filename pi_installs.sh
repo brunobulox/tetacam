8c7b6e5df696dc2d3dce2f20235b54e96cb868d1
Mark Haislip <brunobulox@yahoo.com>
2014-01-10 20:54:27 +0000

    pi install updates

--- pi_installs.sh
+++ pi_installs.sh
@@ -5,8 +5,16 @@ apt-get install -y  build-essentials sqlite3 libsqlite3 asterisk motion &&
  samba apache2 mysql-server php5 phpmyadmin super python-setuptools python-dev &&
  libmysqlclient-dev
 
-easy_install MySQL-python gdata
-
- 
 # You will need to add 'Include /etc/phpmyadmin/apache.conf'
 #  to /etc/apache2/apache2.conf at the end of the file
+
+
+# To install watchdog you might have to  
+#Edit /etc/init.d/skeleton and copy the LSB header (first ten lines or so) 
+#from there to /etc/init.d/mathkernel  the init setup fails for me unless.
+apt-get install watchdog
+
+
+easy_install MySQL-python gdata
+
+
